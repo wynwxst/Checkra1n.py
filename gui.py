1c89a2be21520e26db954b614553a2bf54489248
Sakurai07 <cool5tarXV@gmail.com>
2021-03-10 10:42:35 +0800

    added term

--- gui.py
+++ gui.py
@@ -10,7 +10,29 @@ window.geometry('1800x1800')
 def run():
     os.system('python ipwndfu -p')
 
-btn = Button(window, text="Jailbreak", bg="black", fg="white",command=run)
-btn.grid(column=1, row=0)
+def term():
+    os.system('python term.py')
 
+btn = Button(window, text="Jailbreak", bg="black", fg="white",command=run)
+btn.place(x=0, y=0)
+
+class Lol(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master = master
+
+        menu = Menu(self.master)
+        self.master.config(menu=menu)
+
+        fileMenu = Menu(menu)
+        fileMenu.add_command(label="Terminal", command=term)
+        fileMenu.add_command(label="Exit", command="")
+        menu.add_cascade(label="Open", menu=fileMenu)
+
+        editMenu = Menu(menu)
+        editMenu.add_command(label="Undo")
+        editMenu.add_command(label="Redo")
+        menu.add_cascade(label="Edit", menu=editMenu)
+
+app = Lol(window)
 window.mainloop()
